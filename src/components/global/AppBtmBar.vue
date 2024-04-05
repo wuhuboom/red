1ff368029cb0e92b7f801a040875c2c261b7a698
attoote <119163844+attoote@users.noreply.github.com>
2024-04-05 21:15:43 +0700

    fix

--- src/components/global/AppBtmBar.vue
+++ src/components/global/AppBtmBar.vue
@@ -2,28 +2,16 @@
   <div class="app-btm-bar">
     <ul class="app-btm-list max-width750">
       <li
-        :class="{
-          active: $router.currentRoute.name == item.name || item.active,
-        }"
+        :class="{}"
         class="btm-item center-center"
         v-for="(item, idx) in list"
         @click="goPage(item)"
         :key="idx"
       >
         <p>
-          <img
-            :src="
-              $router.currentRoute.name == item.name || item.active
-                ? item.icon1
-                : item.icon
-            "
-            alt=""
-          />
+          <img :src="item.icon" alt="" />
         </p>
-        <p
-          class="text"
-          v-if="$router.currentRoute.name == item.name || item.active"
-        >
+        <p class="text">
           {{ item.text }}
         </p>
       </li>
@@ -40,28 +28,28 @@ export default {
     return {
       list: [
         {
-          icon: require("@/assets/img/btm.png"),
+          icon: require("@/assets/img/red/btm.png"),
           icon1: require("@/assets/img/btmv.png"),
           text: i18n.t("tabar.home"),
           name: "home",
           active: false,
         },
         {
-          icon: require("@/assets/img/btm1.png"),
+          icon: require("@/assets/img/red/btm1.png"),
           icon1: require("@/assets/img/btm1v.png"),
           text: i18n.t("tabar.match"),
           name: "List",
           active: false,
         },
         {
-          icon: require("@/assets/img/btm2.png"),
+          icon: require("@/assets/img/red/btm2.png"),
           icon1: require("@/assets/img/btm2v.png"),
-          text: i18n.t("tabar.bet"),
+          text: i18n.t("home.order.535219-0"),
           name: "order",
           active: false,
         },
         {
-          icon: require("@/assets/img/btm3.png"),
+          icon: require("@/assets/img/red/btm3.png"),
           icon1: require("@/assets/img/btm3v.png"),
           text: i18n.t("tabar.me"),
           active: false,
@@ -98,6 +86,9 @@ export default {
   .app-btm-list {
     height: 55px;
   }
+  .btm-item {
+    flex-direction: column;
+  }
   .app-btm-list {
     display: flex;
     justify-content: space-around;
@@ -107,13 +98,15 @@ export default {
     left: 0;
     right: 0;
     z-index: 998;
-    background-color: #fff;
+    background-color: var(--bg-body);
     font-size: 12px;
+
     .text {
-      margin-left: 6px;
+      margin-top: 6px;
+      color: var(--primary);
     }
     li {
-      height: 38px;
+      // height: 38px;
       text-align: center;
       border-radius: 19px;
       min-width: 86px;
