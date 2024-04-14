e98411871edc3e375497d66fb4b251071e83a61c
attoote <119163844+attoote@users.noreply.github.com>
2024-04-14 17:31:07 +0700

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: [
+    ["@vue/cli-plugin-babel/preset"],
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          browsers: ["IE >= 11", "Android >= 4.4"],
+        },
+      },
+    ],
+  ],
   plugins: [
     [
       "import",
