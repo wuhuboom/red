0a41ad18f8bf34f061deb8d54c0f34bc4c3d8686
attoote <119163844+attoote@users.noreply.github.com>
2024-04-29 15:07:13 +0700

    version:0.1.5 2024/04/29 15:07:13

--- incrementVersion.js
+++ incrementVersion.js
@@ -1,7 +1,20 @@
+const { exec } = require("child_process");
 const semver = require("semver");
 const fs = require("fs");
 const path = require("path");
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
 
+// 启用插件
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+// 设置时区为北京时间
+dayjs.tz.setDefault("Asia/Shanghai");
+
+// 获取当前时间并格式化
+const beijingTime = dayjs().format("YYYY/MM/DD HH:mm:ss");
 // 更新 VUE_APP_VERSION 环境变量
 const envFilePath = path.resolve(__dirname, ".env");
 // 读取已存在的环境变量
@@ -23,6 +36,37 @@ try {
   // 写入文件
   fs.writeFileSync(envFilePath, envContent);
   console.log(`Version updated from ${oldVersion} to ${newVersion}`);
+
+  exec(`git add .env`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+
+    // 提交代码
+    exec(
+      `git commit -m "version:${newVersion} ${beijingTime}"`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        // 推送到远程仓库
+        exec(`git push`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.log(`stdout: ${stdout}`);
+          console.error(`stderr: ${stderr}`);
+        });
+      }
+    );
+  });
 } catch (err) {
   console.error("Error reading .env file:", err);
 }
