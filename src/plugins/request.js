7eae56d676f3075e8d1329851e3eac955b448fdd
attoote <119163844+attoote@users.noreply.github.com>
2024-05-22 20:39:05 +0700

    version:18 2024/05/22 20:39:05

--- src/plugins/request.js
+++ src/plugins/request.js
@@ -40,7 +40,7 @@ instance.interceptors.response.use(
     const result = res.data || {};
     let { code, msg } = result;
     if ([500].includes(code)) {
-      msg = "";
+      msg = "system  fai";
     }
     if (code === 402) {
       app.$toast.clear();
